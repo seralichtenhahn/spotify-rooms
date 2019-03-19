735e7f7797a00e2e4f1273be1d5882fce65f94bf
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-03-19 10:51:58 +0100

    redirect user back to homepage when no code parameter is provied

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,9 @@
-export default function({ store, redirect }) {
+export default function({ store, redirect, query }) {
   if (store.state.user.loggedIn) {
     return
+  }
+
+  if (!query.code) {
+    redirect("/")
   }
 }
