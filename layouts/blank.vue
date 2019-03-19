9fab19a50d76cf1b5b64a7b2fdcdf2212f7581b8
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-03-19 10:41:27 +0100

    setup auth

---
+++ layouts/blank.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app--layout">
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ["anonymous"]
+}
+</script>
+
+<style lang="scss">
+.app--layout {
+  position: relative;
+  height: 100%;
+  min-height: 100vh;
+
+  .row {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    padding: rem(32 16);
+    text-align: center;
+  }
+}
+</style>
