0b63456cc7b8fceef30c5a1f48072a845c994846
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-03-19 10:47:17 +0100

    setup rooms page

---
+++ pages/rooms/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <h1>
+      Rooms
+    </h1>
+    <p>
+      <strong>User:</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
