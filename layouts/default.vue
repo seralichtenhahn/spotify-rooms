795606f4c4c1a72192b5d40237bd2d862a6acb14
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-03-19 10:46:58 +0100

    use styling from intro page on default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,32 @@
 <template>
-  <div>
+  <div class="app--layout">
     <nuxt/>
   </div>
 </template>
 
-<style>
-/* stylelint-disable */
-html {
-  box-sizing: border-box;
-  font-size: 16px;
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+<script>
+export default {
+  middleware: ["auth"]
 }
+</script>
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+<style lang="scss">
+.app--layout {
+  position: relative;
+  height: 100%;
+  min-height: 100vh;
 
-.button--green {
-  display: inline-block;
-  padding: 10px 30px;
-  border: 1px solid #3b8070;
-  border-radius: 4px;
-  color: #3b8070;
-  text-decoration: none;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  padding: 10px 30px;
-  border: 1px solid #35495e;
-  border-radius: 4px;
-  margin-left: 15px;
-  color: #35495e;
-  text-decoration: none;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  .row {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    padding: rem(32 16);
+    text-align: center;
+  }
 }
 </style>
