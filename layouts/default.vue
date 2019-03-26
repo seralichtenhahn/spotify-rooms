9e884c004ddd37b33454fff3c6a3701ee0ca94c0
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-03-26 11:49:22 +0100

    update layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,52 +21,7 @@ export default {
 
 <style lang="scss">
 .app--layout {
-  position: relative;
-  height: 100%;
-  min-height: 100vh;
-
-  .row {
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 100%;
-    height: 100%;
-    padding: rem(32 16);
-    text-align: center;
-
-    .queue {
-      overflow: scroll;
-    }
-  }
-
-  .topbar {
-    position: absolute;
-    display: flex;
-    width: 100%;
-    height: rem(100);
-
-    .left {
-      align-self: flex-start;
-    }
-  }
-
-  ul {
-    margin: 0;
-    list-style: none;
-
-    li {
-      box-sizing: border-box;
-      padding: rem(12);
-      border-bottom: rem(1) solid $black;
-
-      strong {
-        display: block;
-        font-size: rem(18);
-      }
-    }
-  }
+  min-height: 100%;
+  background-color: $grey-cod;
 }
 </style>
