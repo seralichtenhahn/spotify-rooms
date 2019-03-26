5e0616b0ede1a203bbbb1ab9809b99cd16fb69b9
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-03-26 14:13:45 +0100

    add styles for add track page

--- pages/rooms/_id/add.vue
+++ pages/rooms/_id/add.vue
@@ -1,35 +1,63 @@
 <template>
-  <div class="row">
+  <div class="add-track--page">
     <div class="topbar">
-      <div class="left">
+      <div class="topbar--item left">
         <nuxt-link :to="{ name: 'rooms-id' }">
-          X
+          <IconArrowBack class="icon" />
         </nuxt-link>
       </div>
+      <div class="topbar--item center">
+        Track hinzufügen
+      </div>
+      <div class="topbar--item right"/>
     </div>
-    <div>
-      <h1>Add Song</h1>
-      <input
-        v-model="query"
-        type="text" 
-      >
-      <ul>
-        <li
-          v-for="song in results"
-          :key="song.uri"
-          @click="addSong(song)"
+    <div class="add-track--page--header">
+      <div class="row--inner">
+        <input
+          v-model="query"
+          type="search"
+          class="large"
+          placeholder="Suchen"
         >
-          <strong>{{ song.name }}</strong> by {{ song.artists[0].name }}
-        </li>
-      </ul>
+      </div>
+    </div>
+    <div class="add-track--page--results">
+      <div class="row--outer">
+        <ul>
+          <li class="title">
+            <h3>{{ title }}</h3>
+          </li>
+          <li
+            v-for="song in results"
+            :key="song.uri"
+            @click="addSong(song)"
+          >
+            <img
+              :src="song.album.images[1].url"
+              :alt="song.name"
+            >
+            <div>
+              <h4>{{ song.name }}</h4>
+              <p class="small">{{ song.artists[0].name }}</p>
+            </div>
+            <IconAddCircle class="icon" />
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import debounce from "lodash/debounce"
+import IconArrowBack from "@/assets/icons/arrow-back.svg"
+import IconAddCircle from "@/assets/icons/add-circle-outline.svg"
 
 export default {
+  components: {
+    IconArrowBack,
+    IconAddCircle
+  },
   async asyncData({ redirect, from, route }) {
     if (from.name === route.name) {
       redirect(`/rooms/${route.params.id}`)
@@ -38,6 +66,7 @@ export default {
   data() {
     return {
       query: "",
+      title: "Deine Top Tracks",
       results: []
     }
   },
@@ -61,6 +90,7 @@ export default {
     const { items } = await this.$spotify.getMyTopTracks({
       limit: 10
     })
+    console.log(items)
     this.results = items
   },
   methods: {
@@ -75,6 +105,7 @@ export default {
         limit: 10
       })
       this.results = respone.tracks.items
+      this.title = `Resultate für "${query}"`
     }, 150),
     /**
      * Führt die Action currentRoom/addTrack mit den Track Infos aus
@@ -100,3 +131,61 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.add-track--page {
+  padding-top: rem(50);
+
+  &--header {
+    //
+  }
+
+  &--results {
+    ul {
+      margin: 0;
+      height: calc(100vh - #{rem(80) + rem(130)});
+      overflow: scroll;
+      padding: rem(16);
+      list-style: none;
+      background-color: $grey-mine;
+      border-radius: rem(5);
+
+      li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        img {
+          width: rem(64);
+          height: rem(64);
+        }
+
+        div {
+          flex: 1 1;
+          margin-left: rem(10);
+
+          h4,
+          p {
+            margin: 0;
+            padding: 0;
+          }
+
+          p {
+            color: rgba($white, 0.6);
+          }
+        }
+
+        .icon {
+          fill: rgba($white, 0.6);
+          width: rem(24);
+          height: rem(24);
+        }
+
+        &:not(:last-child) {
+          margin-bottom: rem(10);
+        }
+      }
+    }
+  }
+}
+</style>
