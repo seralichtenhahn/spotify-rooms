af28090de95c72e69a4c8e2323ba837a8a9d3224
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-03-27 11:59:32 +0100

    update nuxt loadingbar color

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,14 +27,13 @@ module.exports = {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: pkg.description }
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: "#FFFFFF" },
+  loading: { color: "#1db954" },
 
   /*
   ** Global CSS
