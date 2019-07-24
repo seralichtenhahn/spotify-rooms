3b27b2a11baa8a649c87a06f4c138f58c82de3ec
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-07-24 15:46:29 +0200

    add addTrack function

---
+++ functions/addTrack.js
@@ -0,0 +1,27 @@
+const spotifyApi = require("./utils/spotify")
+const admin = require("./utils/admin")
+
+const db = admin.firestore()
+
+exports.handler = async function(snap, context) {
+  const roomName = context.params.room
+  const { uri } = snap.data()
+
+  const room = await db
+    .collection("rooms")
+    .doc(roomName)
+    .get()
+
+  const { owner_id: username, playlistId } = room.data()
+
+  const user = await db
+    .collection("users")
+    .doc(username)
+    .get()
+
+  const { accessToken } = user.data()
+
+  spotifyApi.setAccessToken(accessToken)
+
+  await spotifyApi.addTracksToPlaylist(playlistId, [uri])
+}
