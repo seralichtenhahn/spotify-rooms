c16b3b5c40264dbf62ec35644a21ef6305275188
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-07-30 14:54:38 +0200

    add logout route

---
+++ pages/logout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row--inner">
+    <h1>
+      Logout
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch({ redirect, store }) {
+    store.commit("user/setLoginStatus", false)
+
+    return redirect("/")
+  }
+}
+</script>
